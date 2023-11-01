738875331ed94808f3a61d95670db99d6636dcd0
Username <e@email.mail>
2023-11-01 12:37:00 +0200

    add .eslintrc

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import cls from './Sidebar.module.scss'
 import { useState } from 'react'
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher'
+import { Button } from 'shared/ui/Button/Button'
 
 interface SidebarProps {
   className?: string
@@ -19,7 +20,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
         className,
       ])}
     >
-      <button onClick={onToggle}>Toggle</button>
+      <Button onClick={onToggle}>Toggle</Button>
       <div className={cls.switchers}>
         {' '}
         <ThemeSwitcher />
